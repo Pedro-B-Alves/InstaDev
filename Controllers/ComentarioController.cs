e7cb0abb0f4afbad70e66c30a2f5ae19c97098ce
FIC <FIC@info.local>
2021-02-05 11:37:38 -0200

    Resolução de Problemas

--- Controllers/ComentarioController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace Back_End.Controllers
-{
-    public class ComentarioController : Controller
-    {
-        
-    }
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# InstaDev
-Projeto SENAI.
