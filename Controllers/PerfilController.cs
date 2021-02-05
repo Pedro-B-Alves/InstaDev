e189588d3c8fdfd48b8ba4c523227d0c908f73a7
matheus <matheusviniciusdavid@gmail.com>
2021-02-04 22:35:22 -0300

    Dificuldade para criar perfilController

--- Controllers/PerfilController.cs
+++ Controllers/PerfilController.cs
@@ -1,20 +1,29 @@
+using System.Collections.Generic;
 using InstaDev.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InstaDev.Controllers
 {
-    [Route("Perfil")]
+    [Route("Perfil-@viewBag.Usuario.Username")]
     public class PerfilController : Controller 
     {
         Usuario usuarioModel = new Usuario();
         Publicacao publicacaoModel = new Publicacao();
         public IActionResult Perfil()
         {
-            //HttpContext.Session.GetString("_IdUsuario");
-            //ViewBag.Usuario = usuarioModel.Mostrar();
+            
+            HttpContext.Session.GetString("_IdUsuario");
+
+            ViewBag.Usuario = usuarioModel.Mostrar();
+            ViewBag.Publicao = publicacaoModel.ListarPublicacao();
             
             return View();
         }
+
+
+
+
     }
+
 }
