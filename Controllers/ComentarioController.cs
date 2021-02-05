c42b1a1dc31cd47ed911c0a377fd825d282b77d5
unknown <matheusps1407@gmail.com>
2021-02-05 09:41:07 -0300

    Back-End enviado para a nuvem

---
+++ Controllers/ComentarioController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Back_End.Controllers
+{
+    public class ComentarioController : Controller
+    {
+        
+    }
+}
---
+++ Controllers/PublicacaoController.cs
@@ -0,0 +1,14 @@
+using Back_End.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Back_End  
+{
+    public class PublicacaoController
+    {
+
+        Publicacao publicacao = new Publicacao();
+
+
+
+    }
+}
